8794a51bf464d7a9c28b9a9842d62d05a7825b0e
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-05-19 21:18:00 -0300

    Creating show template

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,29 @@ class ProductsController < ApplicationController
     @products
   end
 
+  def show
+    @product = Product.find(params[:id])
+
+    @product
+  end
+
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
   end
 end
